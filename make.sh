50447f040440043e4ee99760ad9eadcbe56d91ff
wut <macwu1992@hotmail.com>
2025-06-29 13:03:50 +0800

    Change make.sh to support multiple folders within one lab

--- make.sh
+++ make.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/bash
 
 current_script_dir=$(realpath "$0" | sed 's|\(.*\)/.*|\1|')
-filenames=$(find ./lab_* -name '*.c' | xargs basename)
 xv6PkgName=$1
 
 if [[ -z "$xv6PkgName" ]]; then
     xv6PkgName="xv6-labs-2020"
 fi
 
-appendLineNum=$(grep -n "UPROGS=" $current_script_dir/$xv6PkgName/Makefile | awk -F":" '{print $1}')
-for file in $(find ./lab_* -name '*.c');
+cFiles=$(find ./lab_* -name '*.c') && cFiles=(${cFiles})
+
+for file in ${cFiles[@]};
 do
+    echo -E "copying $file to $current_script_dir/$xv6PkgName/user/"
     cp $file $current_script_dir/$xv6PkgName/user/
 done
 
-for filename in $filenames;
+filenames=()
+for filename in ${cFiles[@]};
+do
+    filename=$(basename $filename)
+    filenames+=($filename)
+done
+
+appendLineNum=$(grep -n "UPROGS=" $current_script_dir/$xv6PkgName/Makefile | awk -F":" '{print $1}')
+for filename in ${filenames[@]};
 do
     makeProgName=_${filename/.c/}
     # Example for appendLine 153s/$/\n\t\$U\/_sleep.c\\/
@@ -24,9 +33,9 @@ do
     appendLineNum=$((appendLineNum + 1))
 done
 
-cd $current_script_dir/$xv6PkgName/ && rm fs.img && make qemu
+cd $current_script_dir/$xv6PkgName/ && make qemu
 
-for filename in $filenames;
+for filename in ${filenames[@]};
 do
     rm $current_script_dir/$xv6PkgName/user/$filename
 done
